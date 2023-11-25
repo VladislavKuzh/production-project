918f3cf7abd08bc5d1eee2586d67158bf60b641e
VladislavKuzh <v.s.kuzheliev@gmail.com>
2023-11-25 14:48:27 +0200

    feat: add react-router-dom

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './index.scss';
+import { Link, Route, Routes } from 'react-router-dom';
+import { AboutPageAsync }  from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Suspense } from 'react';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />}/>
+          <Route path={'/'} element={<MainPageAsync />}/>
+        </Routes>
+      </Suspense>
+    </div>
+  )
+}
+
+export default App
