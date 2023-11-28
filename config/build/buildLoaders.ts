17dd4c3aef3c4ccd8c3ee73db314f31971da7462
VladislavKuzh <v.s.kuzheliev@gmail.com>
2023-11-28 19:07:44 +0200

    feat: add babel; add i18next extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,6 +9,26 @@ export function buildLoaders({isDev}: BuildOptions): RuleSetRule[] {
         use: ['@svgr/webpack'],
     }
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract", 
+                        {
+                            locales: ['en', 'ua'],
+                            keyAsDefaultValue: true      
+                        }
+                    ],
+                ],
+            }
+        }
+    }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -47,6 +67,7 @@ export function buildLoaders({isDev}: BuildOptions): RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader
     ]
