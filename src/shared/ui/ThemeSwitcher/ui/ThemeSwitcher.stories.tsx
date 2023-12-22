7be4288c1d5954994c1c475574972b8797648650
VladislavKuzh <v.s.kuzheliev@gmail.com>
2023-12-22 11:08:14 +0200

    feat: add Storybook; add stories for  Button, Sidebar, Navbar, AppLink, Loader, ThemeSwitcher, AboutPage, MainPage, NotFoundPage

---
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ThemeSwitcher } from './ThemeSwitcher';
+
+const meta = {
+    title: 'shared/ThemeSwitcher',
+    component: ThemeSwitcher,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof ThemeSwitcher>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Normal: Story = {
+    args: {},
+};
+
+export const Dark: Story = {
+    args: {},
+    decorators: [ThemeDecorator(Theme.DARK)],
+};
